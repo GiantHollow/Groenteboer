732d8127725e5a42298584afb58e27ed89e44548
max <max.vandewege@xs4all.nl>
2019-06-26 13:28:15 +0200

    sql added!

--- Groenteboer/ucProductDisplay.Designer.cs
+++ Groenteboer/ucProductDisplay.Designer.cs
@@ -49,9 +49,9 @@
             this.lblProductNaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProductNaam.Location = new System.Drawing.Point(3, 123);
             this.lblProductNaam.Name = "lblProductNaam";
-            this.lblProductNaam.Size = new System.Drawing.Size(84, 16);
+            this.lblProductNaam.Size = new System.Drawing.Size(106, 16);
             this.lblProductNaam.TabIndex = 1;
-            this.lblProductNaam.Text = "Sample Text";
+            this.lblProductNaam.Text = "Sample Text boi";
             // 
             // lblPrijs
             // 
