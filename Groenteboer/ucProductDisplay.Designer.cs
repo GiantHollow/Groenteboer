1d9f2cb7ecab34d17f53c5a8d5e5b2b1b87c0655
max <max.vandewege@xs4all.nl>
2019-06-26 13:17:10 +0200

    Add project files.

---
+++ Groenteboer/ucProductDisplay.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Groenteboer
+{
+    partial class ucProductDisplay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblProductNaam = new System.Windows.Forms.Label();
+            this.lblPrijs = new System.Windows.Forms.Label();
+            this.btnWinkelmand = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 116);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblProductNaam
+            // 
+            this.lblProductNaam.AutoSize = true;
+            this.lblProductNaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductNaam.Location = new System.Drawing.Point(3, 123);
+            this.lblProductNaam.Name = "lblProductNaam";
+            this.lblProductNaam.Size = new System.Drawing.Size(84, 16);
+            this.lblProductNaam.TabIndex = 1;
+            this.lblProductNaam.Text = "Sample Text";
+            // 
+            // lblPrijs
+            // 
+            this.lblPrijs.AutoSize = true;
+            this.lblPrijs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrijs.Location = new System.Drawing.Point(3, 151);
+            this.lblPrijs.Name = "lblPrijs";
+            this.lblPrijs.Size = new System.Drawing.Size(37, 16);
+            this.lblPrijs.TabIndex = 2;
+            this.lblPrijs.Text = "Prijs:";
+            // 
+            // btnWinkelmand
+            // 
+            this.btnWinkelmand.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWinkelmand.Location = new System.Drawing.Point(4, 170);
+            this.btnWinkelmand.Name = "btnWinkelmand";
+            this.btnWinkelmand.Size = new System.Drawing.Size(115, 48);
+            this.btnWinkelmand.TabIndex = 3;
+            this.btnWinkelmand.Text = "Toevoegen aan winkelmand";
+            this.btnWinkelmand.UseVisualStyleBackColor = true;
+            // 
+            // ucProductDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnWinkelmand);
+            this.Controls.Add(this.lblPrijs);
+            this.Controls.Add(this.lblProductNaam);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ucProductDisplay";
+            this.Size = new System.Drawing.Size(123, 221);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblProductNaam;
+        private System.Windows.Forms.Label lblPrijs;
+        private System.Windows.Forms.Button btnWinkelmand;
+    }
+}
