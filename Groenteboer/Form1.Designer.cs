fad47ca2e9f368884fab7be6ecc08f4c63936c24
Dieter Koert <dieterkoert@gmail.com>
2019-06-27 11:52:09 +0200

    UC in programma gekregen. Graag de UC rebuilding als je er iets aan veranderd.

--- Groenteboer/Form1.Designer.cs
+++ Groenteboer/Form1.Designer.cs
@@ -28,20 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ucProductDisplay1 = new Groenteboer.ucProductDisplay();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ucProductDisplay1
+            // 
+            this.ucProductDisplay1.Location = new System.Drawing.Point(3, 3);
+            this.ucProductDisplay1.Name = "ucProductDisplay1";
+            this.ucProductDisplay1.Size = new System.Drawing.Size(123, 209);
+            this.ucProductDisplay1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.ucProductDisplay1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(2, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(633, 361);
+            this.flowLayoutPanel1.TabIndex = 1;
             // 
             // frmGroenten
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "frmGroenten";
             this.Text = "Groenten";
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private ucProductDisplay ucProductDisplay1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
--- Groenteboer/ucProductDisplay.Designer.cs
+++ Groenteboer/ucProductDisplay.Designer.cs
@@ -32,11 +32,13 @@
             this.lblProductNaam = new System.Windows.Forms.Label();
             this.lblPrijs = new System.Windows.Forms.Label();
             this.btnWinkelmand = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.pictureBox1.Location = new System.Drawing.Point(4, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(115, 116);
@@ -57,32 +59,43 @@
             // 
             this.lblPrijs.AutoSize = true;
             this.lblPrijs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrijs.Location = new System.Drawing.Point(3, 151);
+            this.lblPrijs.Location = new System.Drawing.Point(3, 139);
             this.lblPrijs.Name = "lblPrijs";
-            this.lblPrijs.Size = new System.Drawing.Size(37, 16);
+            this.lblPrijs.Size = new System.Drawing.Size(47, 16);
             this.lblPrijs.TabIndex = 2;
-            this.lblPrijs.Text = "Prijs:";
+            this.lblPrijs.Text = "Prijs: €";
             // 
             // btnWinkelmand
             // 
             this.btnWinkelmand.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnWinkelmand.Location = new System.Drawing.Point(4, 170);
+            this.btnWinkelmand.Location = new System.Drawing.Point(4, 158);
             this.btnWinkelmand.Name = "btnWinkelmand";
             this.btnWinkelmand.Size = new System.Drawing.Size(115, 48);
             this.btnWinkelmand.TabIndex = 3;
             this.btnWinkelmand.Text = "Toevoegen aan winkelmand";
             this.btnWinkelmand.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
             // ucProductDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnWinkelmand);
             this.Controls.Add(this.lblPrijs);
             this.Controls.Add(this.lblProductNaam);
             this.Controls.Add(this.pictureBox1);
             this.Name = "ucProductDisplay";
-            this.Size = new System.Drawing.Size(123, 221);
+            this.Size = new System.Drawing.Size(123, 209);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,5 +108,6 @@
         private System.Windows.Forms.Label lblProductNaam;
         private System.Windows.Forms.Label lblPrijs;
         private System.Windows.Forms.Button btnWinkelmand;
+        private System.Windows.Forms.Label label1;
     }
 }
