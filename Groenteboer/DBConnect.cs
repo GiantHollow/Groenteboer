023629fa50ec63940edbe3e5594c8423d2310fce
max <max.vandewege@xs4all.nl>
2019-06-28 14:03:49 +0200

    Started working on the final product

--- Groenteboer/DBConnect.cs
+++ Groenteboer/DBConnect.cs
@@ -97,49 +97,5 @@ namespace Groenteboer
                 this.CloseConnection();
             }
         }
-
-        //Select statement
-        public List<string>[] Select()
-        {
-            string query = "SELECT * FROM producten";
-
-            //Create a list to store the result
-            List<string>[] list = new List<string>[7];
-            list[0] = new List<string>();
-            list[1] = new List<string>();
-            list[2] = new List<string>();
-            list[3] = new List<string>();
-            list[4] = new List<string>();
-
-            //Open connection
-            if (this.OpenConnection() == true)
-            {
-                //Create Command
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                //Create a data reader and Execute the command
-                MySqlDataReader dataReader = cmd.ExecuteReader();
-
-                //Read the data and store them in the list
-                while (dataReader.Read())
-                {
-                    list[0].Add(dataReader["id"] + "");
-                    list[1].Add(dataReader["naam"] + "");
-                    list[2].Add(dataReader["prijs"] + "");
-                }
-
-                //close Data Reader
-                dataReader.Close();
-
-                //close Connection
-                this.CloseConnection();
-
-                //return list to be displayed
-                return list;
-            }
-            else
-            {
-                return list;
-            }
-        }
     }
 }
